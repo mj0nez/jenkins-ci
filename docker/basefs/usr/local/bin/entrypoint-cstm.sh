cf8f18b22d6b65bad58d088121791c304027d92f
Marcel Johannesmann <mj0nez@fn.de>
2025-10-14 00:22:52 +0200

    feat: allow updating ca certificates on container start

--- docker/basefs/usr/local/bin/entrypoint-cstm.sh
+++ docker/basefs/usr/local/bin/entrypoint-cstm.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ -n "$UPDATE_CA_CERTS" ]]; then
+    update-ca-certificates
+fi
+
 # start the docker process but give it some time to come up
 /usr/local/bin/dockerd-entrypoint.sh &
 sleep 5 && exec "$@"
